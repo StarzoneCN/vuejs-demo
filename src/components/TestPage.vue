f10634347e336c7c2268814026d2fc12953af9f0
LiHongxing <lihongxing@bluemoon.com.cn>
2019-01-21 15:55:33 +0800

    过滤器

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -2,12 +2,27 @@
   <div>
     这是一个测试页
     <p>{{_globalConstans.globalData0}}</p>
+    测试一下过滤器
+    <p>{{ newDate | getDate}}</p>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue';
+  let getDate = function (_d) {
+    return _d.getDate();
+  };
+
+  Vue.filter("getDate", getDate);
   export default {
-    name: "test-page"
+    name: "test-page",
+    data: function () {
+      return {
+        newDate: new Date()
+      }
+    },
+    methods: {
+    }
   }
 </script>
 
